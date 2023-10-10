c3a29e74ae3203c0f18770ae240c14a3704dbc7f
williamrodriprof <william.rodri.prof@gmail.com>
2023-10-10 20:27:16 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    aws = ">= 4.0"
+  }
+}
+
+provider "aws" {
+  profile                  = "default"
+  shared_credentials_files = ["C:/Users/Dell/.aws/credentials"]
+  region                   = "sa-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
